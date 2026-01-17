5770c76662decce60bfa0e4c26a1bd17e0098a5d
colby-campbell <colbycampbell8731@gmail.com>
2026-01-17 15:40:23 -0700

    fix: trying to fix routing issues with the webpage

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/timeblind/',
+  base: '/colby-campbell.github.io/timeblind/',
 })
